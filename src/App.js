44e39eba6c7aa3caef1321fd45c934ea49ab2243
Monica McLaughlan <monica.mclaughlan@gmail.com>
2021-05-21 16:48:10 -0400

    feature findplants

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import {Route, Link, Switch} from "react"
+import FindPlants from './components/FindPlants/FindPlants'
 
 function App() {
+
   return (
     <div className="App">
-
+      
+      <FindPlants/>
+    
     </div>
   );
 }
