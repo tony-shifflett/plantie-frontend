039fd5b57b3df7f23d58989e92b2733b9441a3ac
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-05-21 18:01:16 -0400

    add splash skeleton

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './App.css';
 import {Route, Link, Switch} from "react"
 import FindPlants from './components/FindPlants/FindPlants'
+import Splash from "./components/Splash/Splash"
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Splash />
       <FindPlants/>
     
     </div>
---
+++ src/components/Splash/Splash.js
@@ -0,0 +1,27 @@
+import Logo from "./images/icons8-plant-96.png";
+
+const ButtonSection = {
+  display: "flex",
+  flexDirection: "column",
+};
+
+const ButtonStyle = {
+  backgroundColor: "#E9E9E9",
+  borderRadius: "5px",
+  border: "1px solid gray",
+};
+
+const Splash = () => {
+  return (
+    <div>
+      <img src={Logo} alt="plant logo" />
+      <h1>Plantie</h1>
+      <div>
+        <button style={ButtonStyle}>Sign Up</button>
+        <button style={ButtonStyle}>Log In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Splash;
