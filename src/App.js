2a48666797f6d04b1b3bcfccc98d526710a96fd1
Monica McLaughlan <monica.mclaughlan@gmail.com>
2021-05-24 15:31:35 -0400

    added Routes to App and Link to findplants in myplants

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './App.css';
-import {Route, Link, Switch} from "react"
+import {Route, Link, Switch} from "react-router-dom"
 
 // font awesome imports 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faHome, faCalendar, faSpa, faUser} from '@fortawesome/free-solid-svg-icons';
-import Profile from './components/UserProfile/Profile'
+// import Profile from './components/UserProfile/Profile'
 import FindPlants from './components/FindPlants/FindPlants'
 import Splash from "./components/Splash/Splash"
 import Footer from './components/Footer/Footer'
 import Calendar from './components/Calendar/Calendar'
+import MyPlants from './components/MyPlants';
+import plantSeed from './plantSeed'
 
 // library function to add icons
 library.add(faHome, faCalendar, faSpa, faUser)
@@ -18,12 +20,13 @@ function App() {
 
   return (
     <div className="App">
-      <Splash />
-
-      <Calendar />
-      <Profile/>
-
-      <FindPlants/>
+      <Switch>
+        <Route exact path='/' render={(rp) => <Splash {...rp} />} />
+        <Route exact path='/MyPlants' render={(rp) => <MyPlants {...rp} myPlants={plantSeed}/>} />
+        <Route exact path='/Calendar' render={(rp) => <Calendar {...rp} />} />
+        {/* <Profile/> */}
+        <Route exact path='/FindPlants' render={(rp) => <FindPlants {...rp} />} />
+      </Switch>
       <Footer/>
     </div>
   );
