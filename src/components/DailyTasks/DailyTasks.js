470af049cf9c862f89584e27c18a004d7940e721
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-05-27 14:01:34 -0400

    footer styling complete

--- src/components/DailyTasks/DailyTasks.js
+++ src/components/DailyTasks/DailyTasks.js
@@ -1,25 +1,37 @@
-import React from "react";
-
+import React, { useState } from "react";
+//store import
+import store, { getPlants } from "../../store";
+import {
+  format,
+  startOfWeek,
+  addDays,
+  startOfMonth,
+  endOfMonth,
+  endOfWeek,
+  isSameDay,
+  isSameMonth,
+} from "date-fns";
+import addMonths from "date-fns/addMonths";
+import subMonths from "date-fns/subMonths";
+import toDate from "date-fns/toDate";
 const DailyTasks = (props) => {
   //test schema?
-  const plantList = [
-    {
-      name: "Lenora",
-      status: "Watering time",
-      amount: "150mL",
-    },
-  ];
-
-  const newPlant = plantList.map((plant, index) => {
+  // const plantList = [
+  //   {
+  //     name: "Lenora",
+  //     status: "Watering time",
+  //     amount: "150mL",
+  //   },
+  // ];
+  const newPlant = store.getState().map((plant, index) => {
     return (
       <div key={index}>
-        <h1>{plant.name}</h1>
-        <p>{plant.status}</p>
-        <p>{plant.amount}</p>
+        <h1>{plant.type}</h1>
+        <p>Time to water</p>
+        <p>{plant.water}</p>
       </div>
     );
   });
-
   const Card = {
     padding: "2px 16px",
     boxShadow: "1px 1px 7px 0px rgba(0,0,0,0.4)",
@@ -27,27 +39,25 @@ const DailyTasks = (props) => {
     backgroundColor: "#F8F8F8",
     borderRadius: "15px",
   };
-
   const Container = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
   };
-
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const date = () => {
+    const dateFormat = "MMMM d yyyy";
+    return <h2>{format(currentDate, dateFormat)}</h2>;
+  };
   return (
     <>
       <h1>Today's tasks</h1>
-      <h2>Sun, May 23rd</h2>
+      <div>{date()}</div>
       <div style={Container}>
         <div style={Card}>{newPlant}</div>
       </div>
     </>
   );
 };
-
 export default DailyTasks;
-
-// notes for next meeting-
-// in wireframe it looks like individual tasks have properties themselves - like plant name,
-// watering time, and amount of water
