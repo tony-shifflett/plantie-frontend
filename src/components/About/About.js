71acb76f6096d04fd38225918a53e8e3d99894ed
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-05-27 15:21:58 -0400

    adjust height and icon spacing

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const About = (props) => {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+};
+
+export default About;
